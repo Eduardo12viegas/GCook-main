23f2cd42f34360b56fe6c5236ea119a78b3353e4
Eduardo12viegas <eduardo.viegas@etec.sp.gov.br>
2024-09-05 12:22:50 -0300

    05/09

--- GCook/Controllers/HomeController.cs
+++ GCook/Controllers/HomeController.cs
@@ -8,14 +8,30 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        AppDbContext
+    )
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        HomeVM home = new()
+        {
+            Categorias = _context.Categorias
+                .Where(c => c.ExibirHome)
+                .AsNoTracking()
+                .ToList(),
+            Receitas = _context.Receitas
+                .Include(r => r.Categoria)
+                .Include(r => r.Ingredientes)
+                .AsNoTracking()
+                .ToList()
+        };
+        return View(home);
     }
 
     public IActionResult Privacy()
---
+++ GCook/ViewModels/HomeVM.cs
@@ -0,0 +1,7 @@
+namespace GCook.ViewModels
+
+    public class HomeVM
+    {
+        public List<Categoria> Categorias { get; set;}        
+        public List<Receita> Receitas { get; set;}        
+    }
